f5e22bd511601b88310789aa9c6b164bc364b8fc
cameronsmorgan <cameronsmorgan514@gmail.com>
2024-06-03 13:04:52 +0200

    texturing

---
+++ Assets/Scripts/Quests/Quests/DragonQuest.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "DragonQuest", menuName = "Quest/DragonQuest")]
+
+
+public class DragonQuest : Quest
+{
+
+}
